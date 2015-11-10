15de4d74f57994610e64a54fcc80df90c61831f0
Segey <firstthinker1989@gmail.com>
2015-11-10 17:34:56 +0500

    3й комиит

--- js/myJQslider.js
+++ js/myJQslider.js
@@ -7,13 +7,16 @@
 		sWidth = 900,				 // ширина слайда
  		count = 1 ; 				 // счетчик
 	
+	//автопрокрутка
+	setInterval(function(){next.click()},5000);
+	
 	//---переход вперед
 	next.click(function(){
 		if(sl-count>0){
 			cn.animate({"left":'-='+sWidth+'px'},1000);
 			count++;
 		}else if (sl-count===0){
-			cn.append($(".slides").first().clone());
+			cn.append($(".slides").first().clone());		
 			cn.animate({"left":'-='+sWidth+'px'},1000,function(){
 				$(".slides").last().remove();
 				cn.css({"left":0});
@@ -25,14 +28,14 @@
 	//---переход назад 
 	prev.click(function(){		
 		if(sl-count === sl-1){
-			cn.css({"left":"-"+sWidth+"px"});	
 			cn.prepend($(".slides").last().clone());
+			cn.css({"left":"-"+sWidth+"px"});
+			count=2;	
 			cn.animate({"left": "+="+sWidth+"px"},1000,function(){
 				cn.css({"left":"-"+sWidth*(sl-1)+"px"});			
 				$(".slides").first().remove();				
 				count=sl;
 			})
-
 		}else if(sl-count>=0){
 			cn.animate({"left":'+='+sWidth+'px'},1000);
 		count--;}		
