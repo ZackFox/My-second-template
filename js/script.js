be9ed9650614db92824bc129a7b0f597f0213eed
Segey <firstthinker1989@gmail.com>
2016-02-07 23:42:36 +0500

    accordion

---
+++ js/script.js
@@ -0,0 +1,72 @@
+$(document).ready(function(){
+	// автопрокрутка для CSS слайдера
+	var 
+    obj = $(".slider_wrapper_css input[type='radio']"),
+    control = $(".controls label"),
+		firstLabel = $(".controls label").first(),
+		lasttLabel = $(".controls label").last(),
+		SecondLabel = firstLabel.next();
+	
+  setInterval(function(){
+		if (obj.first().prop("checked")){
+      SecondLabel.click();
+    }
+		else if (obj.first().next().prop("checked")){
+      SecondLabel.next().click();
+    }	
+		else if(obj.last().prev().prop("checked")){
+      lasttLabel.click();
+    }
+		else if (obj.last().prop("checked")){
+      firstLabel.click();
+    }	
+	},4000);
+  
+  // ---------------открытие меню
+  var isOpen = false;
+  $(".menu-btn").click(function(){
+    if(Modernizr.cssanimations){  
+      $(".cover").fadeToggle(500);
+      $(".side-menu").toggleClass("menu-open");
+    }
+    // анимация для IE9
+    else {
+      if(!isOpen){
+        isOpen = true;
+        $(".side-menu").animate({"right":"0px"},800);   
+      }           
+      else{   
+        isOpen = false;
+        $(".side-menu").animate({"right":"-260px"},800);  
+      }            
+    }    
+  });
+
+  //----------- аккордион
+  $(".acc-head").click(function(e){
+    e.preventDefault();
+    var $this = $(this);
+
+    if(!$this.hasClass('acc-active')){
+      $('.acc-head').removeClass("acc-active").next(".acc-drop").stop(true,false).slideUp(300);
+      $this.addClass('acc-active').next(".acc-drop").stop(true,false).slideDown(300);
+    }else{
+      $this.removeClass('acc-active').next(".acc-drop").stop(true,false).slideUp(300);
+    }
+  });
+
+  // загрузка текста 
+  $.getJSON("tab.json",function(data){
+    $(".tab-content p").html(data.tab1);
+  });
+  $(".tab-head").click(function(){ 
+    var tab = $(this).data("tab");
+    $.getJSON("tab.json", function(data){
+      $(".tab-content p").html(data[tab]);});
+  }); 
+
+});
+
+
+
+
